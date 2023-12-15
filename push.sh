e2275330480f1fafbb71cd151d2531832c9b6594
chibhu <nshastryk13@gmail.com>
2023-12-15 23:34:15 +0530

    commit done

--- push.sh
+++ push.sh
@@ -6,8 +6,4 @@ git add .
 
 git commit -m "commit done"
 
-git config credential.helper 'store --file=.git-credentials'
-
-echo -e "protocol=https\nhost=github.com\nusername=chibhu\npassword=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" > .git-credentials
-
-git push https://chibhu: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@repository_url main
+git push https://chibhu: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@github.com/chibhu/invitation.git main
